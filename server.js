cdb36d15bb359f9df32d49b29062f8176d6994a9
mandlibharathi <mandlibharathi@gmail.com>
2017-11-28 20:43:23 +0530

    commeted

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 var express=require('express');
-var bodyParser=require('body-parser');
 var mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost/Person')
+var bodyParser=require('body-parser');
 var app=express();
 app.use(bodyParser.json());
-mongoose.connect('mongodb://localhost/Person')
-var db=mongoose.connection;
 var user=require('./sehema')
-var user1=require('./app')
-app.get('/',function(req,res){
-user.getuser(function(req,user){
-    if(err){
-        console.log(err)
-    }
-    else{
-        res.json(user)
-    }
-})
-})
-app.get('/',user1)
-app.listen(8000);
-console.log('hello')
+app.post('/api',function(req,res){
+    var users=req.body;
+user('users',function(err,user){
+if(err){
+    res.json(err)
+}
+else{
+    res.json(user)
+}
+});
+});
+app.listen(7000);
+console.log('eveything is fine now ')
