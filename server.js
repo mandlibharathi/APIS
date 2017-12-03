7c1539af477f60bb00b79a46cb7d2fe316412a20
mandlibharathi <mandlibharathi@gmail.com>
2017-12-03 09:26:46 +0530

    server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express=require('express');
 var bodyParser=require('body-parser');
+var jwt=require('jsonwebtoken');
 var mongoose=require('mongoose');
 var app=express();
 app.use(bodyParser.json());
 var user=require('./router');
-app.use('/router',user)
+app.use('/api',user)
 app.listen(8000);
 console.log('test localhost');
