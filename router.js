f7ea7246384316bd4e078e6b7d907347bb111642
mandlibharathi <mandlibharathi@gmail.com>
2017-12-08 23:06:36 +0530

    router

--- router.js
+++ router.js
@@ -1,8 +1,9 @@
 var express=require('express');
 var router=express.Router();
-var user=require('./schema');
+var User=require('./schema');
+var jwt=require('jsonwebtoken');
 router.get('/signup',function(req,res){
-    user.find({},function(err,result){
+    User.find({},function(err,result){
         if(err){
             res.send(err)
         }
@@ -11,23 +12,53 @@ router.get('/signup',function(req,res){
         }
     })
 })
+router.post('/login',function(req,res){
+     var phone=req.body.phone;
+    var email=req.body.email;
+    var password=req.body.password;
+    User.findOne({$or:[{'phone':phone},{'email':email}]},function(err,user){
+        if(err){
+            res.send(err)
+        }
+        if(!user){
+            res.json({sucess:false,msg:"user cannot find"})
+        }
+        else if(user){
+            if(user.password!=req.body.password){
+                res.json({sucess:false,msg:'wrong pasword'})
+            }
+            else{
+            
+                  res.json({
+                    success: true,
+                    message: 'sucessfully loggedin!',
+                    
+                  });
+            }
+        }
+        
+    })
+})
+
 router.post('/signup',function(req,res){
     var id=req.body.id;
     var phone=req.body.phone;
     var email=req.body.email;
+    var password=req.body.password;
     var role=req.body.role;
     var firstname=req.body.firstname;
-    var lastname=req.body.lastname;
+     var lastname=req.body.lastname;
     var countryCode=req.body.countryCode;
-    var newuser=new user();
-    newuser.id=id;
-    newuser.phone=phone;
-    newuser.email=email;
-    newuser.role=role;  
-    newuser.firstname=firstname;
-    newuser.lastname=lastname;
-    newuser.countryCode=countryCode;
-    user.findOne({$or:[{'phone':phone},{'email':email}]},function(err,user){
+    var newUser=new User();
+    newUser.id=id;
+    newUser.phone=phone;
+    newUser.email=email;
+    newUser.password=password;
+    newUser.role=role;  
+    newUser.firstname=firstname;
+    newUser.lastname=lastname;
+    newUser.countryCode=countryCode;
+    User.findOne({$or:[{'phone':phone},{'email':email}]},function(err,user){
         if(err){
         res.send(err)
         }
@@ -35,9 +66,9 @@ router.post('/signup',function(req,res){
             res.json({msg:'email / phone already taken'})
         }
         else {
-            newuser.save(function(err,savedfile){
+            newUser.save(function(err,savedfile){
                 if(err){
-                    res.staus(500).send()
+                    res.status(500).send()
                 }
                 else{
                     res.status(200).send(savedfile)
@@ -48,4 +79,8 @@ router.post('/signup',function(req,res){
 
 })
 
- module.exports=router;                                                                                                                                               
+
+    
+
+
+ module.exports=router;                                                                                                                                
