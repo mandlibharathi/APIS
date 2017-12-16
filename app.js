be85ad439f0c2905471990b23ed7688e09fd5c59
mandlibharathi <mandlibharathi@gmail.com>
2017-12-16 23:22:47 +0530

    app

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 var express=require('express');
 var app=express();
 var bodyParser=require('body-parser');
+var passport=require('passport')
 var mongoose=require('mongoose');
+var passbook=require('./models/auth')
 mongoose.connect('mongodb://localhost/Person');
 var db=mongoose.connection;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
+
 var user=require('./router');
-app.use('/api',user);
+app.use('/',user);
+app.get('/', function(req, res) {
+    res.render('index.ejs'); 
+});
+app.get('/profile', isLoggedIn, function(req, res) {
+    res.render('profile.ejs', {
+        user : req.user 
+    });
+});
+function isLoggedIn(req, res, next) {
+    
+        if (req.isAuthenticated())
+            return next();
+        res.redirect('/');
+    }
 
 
 app.listen(7000);
