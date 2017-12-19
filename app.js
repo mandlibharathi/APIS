5a9288bc0c09a485840df3ca8390a94ddb2da61a
mandlibharathi <mandlibharathi@gmail.com>
2017-12-19 20:59:23 +0530

    app

--- app.js
+++ app.js
@@ -1,31 +1,53 @@
 var express=require('express');
 var app=express();
+var mongoose=require('mongoose')
 var bodyParser=require('body-parser');
+var cookieParser=require('cookie-parser')
+mongoose.connect('mongodb://localhost/Person')
+var db=mongoose.connection
 var passport=require('passport')
-var mongoose=require('mongoose');
-var passbook=require('./models/auth')
-mongoose.connect('mongodb://localhost/Person');
-var db=mongoose.connection;
+var session=require('express-session')
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(cookieParser())
+app.use(session({
+    secret: 'mysecret',
+    resave: true,
+    saveUninitialized:true
+  }))
+app.use(passport.initialize())
+app.use(passport.session())
+var router=require('./router')
+app.use('/',router);
 
-var user=require('./router');
-app.use('/',user);
-app.get('/', function(req, res) {
-    res.render('index.ejs'); 
-});
-app.get('/profile', isLoggedIn, function(req, res) {
+app.get('/',function(req,res){
+    res.render('index.ejs',{
+        
+    })
+})
+  
+app.get('/profile',isLoggedIn,function(req, res) {
     res.render('profile.ejs', {
-        user : req.user 
+        user : req.user // get the user out of session and pass to template
     });
 });
+
+
 function isLoggedIn(req, res, next) {
     
+        // if user is authenticated in the session, carry on
         if (req.isAuthenticated())
             return next();
+    
+        // if they aren't redirect them to the home page
         res.redirect('/');
     }
 
+    
+   
+   
+   
+    
+    
 
 app.listen(7000);
 console.log('servei is running ......')
