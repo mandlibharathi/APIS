a1861c613df7c1453576ad912533661f5ad7570a
mandlibharathi <mandlibharathi@gmail.com>
2017-12-21 15:41:04 +0530

    app

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var app=express();
 var mongoose=require('mongoose')
 var bodyParser=require('body-parser');
 var cookieParser=require('cookie-parser')
+var User=require('./schema')
+var passport=require('passport')
 mongoose.connect('mongodb://localhost/Person')
 var db=mongoose.connection
-var passport=require('passport')
 var session=require('express-session')
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(session({
     secret: 'mysecret',
@@ -16,38 +18,28 @@ app.use(session({
   }))
 app.use(passport.initialize())
 app.use(passport.session())
+app.get('/', function(req, res) {
+            res.render('index.ejs'); 
+        });
+    
+        app.get('/login',isLoggedIn, function(req, res) {
+                user : req.user 
+            });
+    
+    
+        function isLoggedIn(req, res, next) {
+            
+                if (req.isAuthenticated())
+                    return next();
+                res.redirect('/');
+            }
+            
+
+
+
+
+
 var router=require('./router')
 app.use('/',router);
-
-app.get('/',function(req,res){
-    res.render('index.ejs',{
-        
-    })
-})
-  
-app.get('/profile',isLoggedIn,function(req, res) {
-    res.render('profile.ejs', {
-        user : req.user // get the user out of session and pass to template
-    });
-});
-
-
-function isLoggedIn(req, res, next) {
-    
-        // if user is authenticated in the session, carry on
-        if (req.isAuthenticated())
-            return next();
-    
-        // if they aren't redirect them to the home page
-        res.redirect('/');
-    }
-
-    
-   
-   
-   
-    
-    
-
 app.listen(7000);
-console.log('servei is running ......')
+console.log('server is running ......')
