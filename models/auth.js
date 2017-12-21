8bc0130192e7a585894927c13bf1b35f82f6cd39
mandlibharathi <mandlibharathi@gmail.com>
2017-12-21 15:41:54 +0530

    auth

--- models/auth.js
+++ models/auth.js
@@ -1,8 +1,8 @@
 module.exports={
     "facebookAuth":{
-        "clientID":"135241980484004",
-        "clientSecret":"aa5dec53d87c403896f3a73308f941f7",
+        "clientID":"2279467665613015",
+        "clientSecret":"a5896f503ffe167015e35f9903e62bbb",
         "callbackURL":"http://localhost:7000/auth/facebook/callback",
-    
+        "profileFields":['id','name','displayName','emails']
     }
 }
