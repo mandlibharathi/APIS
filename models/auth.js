a2a76d1b359af82a28ad73db63ec8a36cebb1ede
mandlibharathi <mandlibharathi@gmail.com>
2017-12-22 12:59:12 +0530

    auth.js

--- models/auth.js
+++ models/auth.js
@@ -4,5 +4,11 @@ module.exports={
         "clientSecret":"a5896f503ffe167015e35f9903e62bbb",
         "callbackURL":"http://localhost:7000/auth/facebook/callback",
         "profileFields":['id','name','displayName','emails']
+    },
+    "googleAuth":{
+        "clientID":"896391461574-4tml1v55mqgiduu41v381vt858r811hs.apps.googleusercontent.com",
+        "clientSecret":"2sosl6ofR4RznhoAG97QQt0x",
+        "callbackURL":"http://localhost:7000/auth/google/callback",
+        
     }
 }
